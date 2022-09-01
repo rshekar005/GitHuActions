8394c8bfa31d82b99cd77d5d4e561ccef6443003
rshekar005 <rshekar005@gnail.com>
2022-09-01 15:11:07 +0530

    Added unit-test

---
+++ src/test/java/com/qa/DemoUnitTest.java
@@ -0,0 +1,13 @@
+package com.qa;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoUnitTest {
+
+    @Test
+    public void test1(){
+        int actual =1;
+        Assert.assertEquals(actual,2);
+    }
+}
